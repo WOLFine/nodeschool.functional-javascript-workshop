9a6b7b1d29e09976a6e3e5f1fdbc9b7dea62e494
Asier Barrenetxea <asierba@gmail.com>
2013-11-30 16:18:14 +0000

    Basic Every Some

---
+++ 05-Basic-EverySome.js
@@ -0,0 +1,9 @@
+module.exports = function checkUsersValid(goodUsers) {
+  return function(submittedUsers) {
+  	return submittedUsers.every(function(submitted) {
+  		return goodUsers.some(function (good) {
+  			return submitted.id === good.id;
+  		});
+  	});
+  };
+}
